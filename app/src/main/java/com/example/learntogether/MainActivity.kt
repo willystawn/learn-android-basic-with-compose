0967acf1052017c900bdfbe77f9ca7255ad98ffe
Willy <wilisetiawan29@gmail.com>
2024-03-16 19:48:04 +0700

    initial commit

---
+++ app/src/main/java/com/example/learntogether/MainActivity.kt
@@ -0,0 +1,86 @@
+package com.example.learntogether
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.learntogether.ui.theme.LearnTogetherTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LearnTogetherTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ComposeArticleApp()
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun ComposeArticleApp() {
+    ArticleCard(
+        title = stringResource(R.string.title_jetpack_compose_tutorial),
+        shortDesc = stringResource(R.string.jetpack_compose_short_description),
+        longDesc = stringResource(R.string.jetpack_compose_long_description),
+        imagePainter = painterResource(R.drawable.bg_compose_background)
+    )
+}
+
+@Composable
+fun ArticleCard(
+    title: String,
+    shortDesc: String,
+    longDesc: String,
+    imagePainter: Painter,
+    modifier: Modifier = Modifier
+) {
+    Column (modifier = modifier) {
+        Image(painter = imagePainter, contentDescription = null)
+        Text(
+            text = title,
+            fontSize = 24.sp,
+            modifier = Modifier.padding(16.dp)
+        )
+        Text(
+            text = shortDesc,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier.padding(16.dp, 0.dp)
+        )
+        Text(
+            text = longDesc,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier.padding(16.dp)
+        )
+    }
+}
+
+@Preview(showBackground = false)
+@Composable
+fun LearnTogetherAppPreview() {
+    LearnTogetherTheme {
+        ComposeArticleApp()
+    }
+}
